c5efbfbd9e36803d167b06a53b319bae7b7c3ee7
Shivakant6387 <shivakantsingh900@gmail.com>
2024-06-30 23:29:17 +0530

    comparator

---
+++ src/main/java/org/example/java8/ComparatorLambdaExample.java
@@ -0,0 +1,18 @@
+package org.example.java8;
+
+import java.util.Comparator;
+
+public class ComparatorLambdaExample {
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+        System.out.println("Result of the comparator is :" + comparator.compare(3, 2));
+        Comparator<Integer> comparatorLambda = (Integer a, Integer b) -> a.compareTo(b);
+        System.out.println("Result of the comparator using lambda is :" + comparatorLambda.compare(3, 2));
+
+    }
+}
