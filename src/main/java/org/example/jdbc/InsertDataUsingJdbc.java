5c57eb1de4685700e6b3afc2a2cbdd53d81b9739
Shivakant6387 <shivakantsingh900@gmail.com>
2024-07-05 22:17:54 +0530

    insert data using jdbc

---
+++ src/main/java/org/example/jdbc/InsertDataUsingJdbc.java
@@ -0,0 +1,38 @@
+package org.example.jdbc;
+
+import java.sql.*;
+
+public class InsertDataUsingJdbc {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String url = "jdbc:mysql://localhost:3306/jdbc?useSSL=false";
+        String user = "root";
+        String password = "data#12345";
+        try {
+            //Get a connection to database
+            connection = DriverManager.getConnection(url, user, password);
+            //Create a statement
+            statement = connection.createStatement();
+            //Insert a new Persons
+            System.out.println("Inserting a new Persons to database\n");
+            int rowAffected = statement.executeUpdate("insert into Persons" +
+                    "(PersonID,LastName,FirstName,Address,City) "
+                    + "values" +
+                    "(2,'Mishra','Sakshi','mahoba','mahoba')");
+            resultSet = statement.executeQuery("select * from Persons order by PersonID");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("LastName") + "," + resultSet.getString("FirstName"));
+            }
+
+        } catch (Exception e) {
+            e.getLocalizedMessage();
+            System.out.println(e.getLocalizedMessage());
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+    }
+}
