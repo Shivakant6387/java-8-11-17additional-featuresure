a2f206aeb8ec4300971f5ec1f507cb8fc8be1ebd
Shivakant6387 <shivakantsingh900@gmail.com>
2024-08-20 12:25:40 +0530

    runnable method to implements other way

--- src/main/java/org/example/multithread/ThreadDemo.java
+++ src/main/java/org/example/multithread/ThreadDemo.java
@@ -2,9 +2,23 @@ package org.example.multithread;
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        Thread thread = new Thread(new Runnables());
-        thread.start();
-        Thread thread2 = new Thread(new Runnables());
-        thread2.start();
+//        Thread thread = new Thread(new Runnables());
+//        thread.start();
+//        Thread thread2 = new Thread(new Runnables());
+//        thread2.start();
+        Thread thread1=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Hello" + i);
+                    try {
+                        Thread.sleep(100);
+                    } catch (Exception e) {
+                        e.getLocalizedMessage();
+                    }
+                }
+            }
+        });
+        thread1.start();
     }
 }
